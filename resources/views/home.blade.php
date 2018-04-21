eec28a7e5ede714461340aeb88f68bafc1810da6
pranav <pranavnanduri@gmail.com>
2018-04-21 14:38:19 -0400

    changes in view page of faq

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -56,7 +56,7 @@
 
 
 
-                                                    <a class="btn btn-primary float-right" href="#">
+                                                    <a class="btn btn-primary float-right" href="{{ route('question.show', ['id' => $question->id]) }}">
 
                                                         View
 
